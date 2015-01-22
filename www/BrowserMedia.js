69be026e5af40d484069de5b19dc764c3fdeec64
Ezo Saleh <ezo@the-experimenters.com>
2015-01-22 10:13:50 +0000

    Can now wrap the Media object and use 3rd part libs.

--- www/BrowserMedia.js
+++ www/BrowserMedia.js
@@ -1,3 +1,47 @@
-var exec = require('cordova/exec');
+var exec = require('cordova/exec'),
+    _ = require('com.reallyenglish.cordova.plugin.browser-media.underscorejs');
+
+var extend = function(protoProps, staticProps) {
+  var parent = this;
+  var child;
+
+  // The constructor function for the new subclass is either defined by you
+  // (the "constructor" property in your `extend` definition), or defaulted
+  // by us to simply call the parent's constructor.
+  if (protoProps && _.has(protoProps, 'constructor')) {
+    child = protoProps.constructor;
+  } else {
+    child = function(){ return parent.apply(this, arguments); };
+  }
+
+  // Add static properties to the constructor function, if supplied.
+  _.extend(child, parent, staticProps);
+
+  // Set the prototype chain to inherxit from `parent`, without calling
+  // `parent`'s constructor function.
+  var Surrogate = function(){ this.constructor = child; };
+  Surrogate.prototype = parent.prototype;
+  child.prototype = new Surrogate;
+
+  // Add prototype properties (instance properties) to the subclass,
+  // if supplied.
+  if (protoProps) _.extend(child.prototype, protoProps);
+
+  // Set a convenience property in case the parent's prototype is needed
+  // later.
+  child.__super__ = parent.prototype;
+
+  return child;
+};
+
+var baseMedia = window.Media;
+baseMedia.extend = extend;
+
+window.Media = baseMedia.extend({
+  constructor: function(src, successCallback, errorCallback, statusCallback){
+    console.log('Wrapping this BAD BOY');
+    baseMedia.apply(this, arguments);
+  }
+});
 
 module.exports = window.Media;
