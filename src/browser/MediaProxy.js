28616ef0b55a21e93fb6eb5554b8cedbf52b031e
Ezo Saleh <ezo@the-experimenters.com>
2015-03-13 10:56:55 -0700

    onStatusCallback can now be used through out an object.

--- src/browser/MediaProxy.js
+++ src/browser/MediaProxy.js
@@ -1,6 +1,3 @@
-//TODO: Implement DURATION
-
-// var jQuery = require('com.reallyenglish.cordova.plugin.browser-media.jQuery');
 var jQuery = window.com.reallyenglish.cordova.plugin.browsermedia.lib.jQuery;
 jQuery.noConflict(true);
 
@@ -19,12 +16,14 @@ var MEDIA_NONE = 0,
 
 module.exports = {
   create: function(){
-    var onStatusCallback = Array.prototype.slice.call(arguments, 0, 1)[0],
+    this.onStatusCallback = Array.prototype.slice.call(arguments, 0, 1)[0];
+
+    var self = this,
         args = Array.prototype.slice.call(arguments, 2)[0],
         id = args[0],
         src = args[1];
 
-    console.log('MediaProxy#create onStatusCallback', onStatusCallback);
+    console.log('MediaProxy#create onStatusCallback', self.onStatusCallback);
     console.log('MediaProxy#create id', id, 'src', src);
 
     jQuery( document ).ready(function() {
@@ -34,7 +33,6 @@ module.exports = {
       playerDiv.id = 'jquery_jplayer_' + id;
 
       document.body.appendChild(playerDiv);
-      // http://www.freesfx.co.uk/rx2/mp3s/10/11333_1399100038.mp3
 
       jQuery('#' + playerDiv.id).jPlayer({
         noConflict: 'window.com.reallyenglish.cordova.plugin.browsermedia.lib.jQuery',
@@ -46,27 +44,27 @@ module.exports = {
 
         canplaythrough: function(e){
           console.log('MediaProxy#create CANPLAYTHROUGH event', e);
-          onStatusCallback(id, MEDIA_STATE, MEDIA_CAN_PLAY_THROUGH);
+          self.onStatusCallback(id, MEDIA_STATE, MEDIA_CAN_PLAY_THROUGH);
         },
 
         durationchange: function(e){
           console.log('MediaProxy#create DURATIONCHANGE event', e);
-          onStatusCallback(id, MEDIA_DURATION, e.jPlayer.status.duration);
+          self.onStatusCallback(id, MEDIA_DURATION, e.jPlayer.status.duration);
         },
 
         play: function(e){
           console.log('MediaProxy#create PLAY event', e);
-          onStatusCallback(id, MEDIA_STATE, MEDIA_STARTING);
+          self.onStatusCallback(id, MEDIA_STATE, MEDIA_STARTING);
         },
 
         playing: function(e){
           console.log('MediaProxy#create PLAYING event', e);
-          onStatusCallback(id, MEDIA_STATE, MEDIA_RUNNING);
+          self.onStatusCallback(id, MEDIA_STATE, MEDIA_RUNNING);
         },
 
         pause: function(e){
           console.log('MediaProxy#create PAUSE event', e);
-          onStatusCallback(id, MEDIA_STATE, MEDIA_PAUSED);
+          self.onStatusCallback(id, MEDIA_STATE, MEDIA_PAUSED);
         },
 
         seeking: function(e){
@@ -79,7 +77,7 @@ module.exports = {
 
         ended: function(e){
           console.log('MediaProxy#create ENDED event', e);
-          onStatusCallback(id, MEDIA_STATE, MEDIA_STOPPED);
+          self.onStatusCallback(id, MEDIA_STATE, MEDIA_STOPPED);
         },
 
         wmode:"window",
